8ea108330c7adc740754c83feca2bd6fadd5414a
ArthurDMVM <115365021+ArthurDMVM@users.noreply.github.com>
2023-10-15 22:49:56 -0300

    22:49 15/10/2023

---
+++ index.js
@@ -0,0 +1,30 @@
+function scrollToSection(sectionId) {
+    var element = document.getElementById(sectionId);
+    if (element) {
+      var position = element.offsetTop - 100; // Ajuste o valor 100 para a quantidade de pixels acima do elemento desejado
+      window.scrollTo({
+        top: position,
+        behavior: 'smooth'
+      });
+    }
+  }
+
+  function sobremimPagina(){
+    window.location.href = "/sobremimPagina/sobremim.html";
+  }
+
+  function voltarInicio(){
+    window.location.href = "../index.html";
+  }
+
+  function paginaProjetos(){
+    window.location.href = "/projetosPagina/projetosP.html";
+  }
+
+  function projetosEscola(){
+    window.location.href = "escolaPagina/escola.html";
+  }
+
+  function voltarPagina(){
+    history.go(-1);
+  }
